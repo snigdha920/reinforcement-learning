e3c682a9a527335769e374bb14719b41d5c96800
Snigdha Singh <snigdha920@gmail.com>
2022-03-25 01:46:38 +0530

    imporve padding and spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="min-h-screen bg-mario lg:bg-contain grid grid-cols-2 items-center gap-10">
+      <main className="min-h-screen bg-mario lg:bg-contain grid grid-cols-2 items-center gap-10 px-16 pb-16">
         <PlayButtons setPlayUrl={setPlayUrl} />
-        <ReactPlayer url={playUrl} />
+
+        <ReactPlayer url={playUrl} width="100%" />
       </main>
     </div>
   )
