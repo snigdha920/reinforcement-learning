3fc473d299b8dd4e9422b3d77a3543c02a5a27d3
Snigdha Singh <snigdha920@gmail.com>
2022-03-25 01:32:53 +0530

    try to fix purge 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,14 @@
 module.exports = {
-  purge: [
-    './components/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}'
-  ],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'mario': "url('/bg.png')"
-      }
+        mario: "url('/bg.png')",
+      },
     },
   },
-  variants: {},
-  plugins: [
-    "postcss-import",
-    "tailwindcss",
-    "autoprefixer",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
+  plugins: [],
 }
