16f2393792f1a9cc938c49c2b19d3e03d19c6f08
Snigdha Singh <snigdha920@gmail.com>
2022-03-25 00:09:14 +0530

    add background image

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'mario': "url('/bg.png')"
+      }
+    },
+  },
+  plugins: [],
+}
